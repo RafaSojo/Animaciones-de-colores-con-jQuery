e191792ebda70a605e29a0ecc264f589750d2243
Rafa Sojo <rafasojo98@gmail.com>
2019-02-17 18:41:44 +0100

    Reestructura código

--- js/main.js
+++ js/main.js
@@ -1,29 +1,27 @@
 {
     /**
-     * @author Marcos Gallardo Pérez
+     * @author Marcos Gallardo Pérez y Rafael Sojo Ruiz
      */
     $(init);
-    
+
 
     function init() {
-        $("#ejemplo1").click(function animacion1(){
-            if($(this).css("background-color")==="rgb(0, 0, 0)")
+        $("#ejemplo1").click(function animacion1() {
+            if ($(this).css("background-color") === "rgb(0, 0, 0)")
                 $(this).animate({
                     "width": "300px",
                     "background-color": "red",
-                    "color":"black",
-                    "border-color" : "black"
-                },1000);
+                    "color": "black",
+                    "border-color": "black"
+                }, 1000);
             else
                 $(this).animate({
                     "width": "150px",
                     "background-color": "black",
-                    "color":"white",
-                    "border-color" : "red"
-                },1000);
-
-        })
-      
+                    "color": "white",
+                    "border-color": "red"
+                }, 1000);
+        });
     }
 
 }
